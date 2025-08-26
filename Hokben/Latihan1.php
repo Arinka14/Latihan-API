43a6b439635429d1c49290da232e5f2bc578686b
Arinka14 <arinkawidim1402@gmail.com>
2025-08-26 09:13:37 +0700

    Selesai resolve konflik dan update tampilan menu

--- Hokben/Latihan1.php
+++ Hokben/Latihan1.php
@@ -3,7 +3,7 @@ $data = file_get_contents(__DIR__ . '/Data/Studi.json');
 $menu = json_decode($data, true);
 ?>
 
-<!Doctype html>
+<!DOCTYPE html>
 <html lang="en">
   <head>  
     <meta charset="utf-8">
@@ -31,11 +31,11 @@ $menu = json_decode($data, true);
     <?php foreach ($menu['menu'] as $row): ?>
       <div class="col-md-4">
         <div class="card mb-3">
-         <img src="img/<?= $row['Gambar']; ?>">
+         <img src="img/<?= $row['Gambar']; ?>" class="card-img-top" alt="<?= $row['nama']; ?>" style="max-height: 200px; object-fit: cover;">
           <div class="card-body">
             <h5 class="card-title"><?= $row['nama']; ?></h5>
             <p class="card-text"><?= $row['deskripsi']; ?></p>
-            <h5 class="card-title">Rp <?= $row['harga']; ?></h5>
+            <h5 class="card-title">Rp <?= number_format($row['harga'], 0, ',', '.'); ?></h5>
             <a href="#" class="btn btn-primary">Pesan Sekarang</a>
           </div>
         </div>
